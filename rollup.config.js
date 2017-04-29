74afd79b15c0a3f74a96488df4bf805ad632603d
Cédric Meuter <cedric.meuter@gmail.com>
2017-04-29 14:43:15 +0200

    Update to use rollup-copy-plugin

--- rollup.config.js
+++ rollup.config.js
@@ -6,6 +6,7 @@ import uglify from "rollup-plugin-uglify";
 import livereload from "rollup-plugin-livereload";
 import filesize from "rollup-plugin-filesize";
 import progress from "rollup-plugin-progress";
+import copy from "rollup-plugin-copy";
 
 const NODE_ENV = process.env.NODE_ENV || "development";
 const PROD     = (NODE_ENV === "production");
@@ -17,7 +18,7 @@ console.log();
 
 export default {
     entry: "src/main.jsx",
-    dest: "dist/main.bundle.js",
+    dest: "dist/js/main.bundle.js",
     format: "iife",
     sourceMap: PROD ? false : "inline",
     plugins: [
@@ -39,5 +40,11 @@ export default {
         (PROD && uglify()),
         (WATCH && livereload()),
         (filesize()),
+        copy({
+            "src/index.html": "dist/index.html",
+            "node_modules/bootstrap/dist": "dist/vendor/bootstrap",
+            "node_modules/font-awesome": "dist/vendor/font-awesome",
+            verbose: true
+        })
     ]
 }
